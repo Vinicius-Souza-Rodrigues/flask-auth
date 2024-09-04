a9ab11837cbecb40f37220cad968996f5fbae4f8
Vinícius souza <vinicanal604@gmail.com>
2024-09-04 13:05:05 -0300

    flask v1.4

--- utils/db.py
+++ utils/db.py
@@ -8,5 +8,6 @@ def adicionar(name , username, email, hashed_password):
 
     account_info = {"saldo" : 1.0, "user_id" : current_id}
     
+    #DAO
     db.append({"name" : name, "username" : username, "email" : email, "hashed_password" : hashed_password, "account_info" : account_info})
     print(db)
